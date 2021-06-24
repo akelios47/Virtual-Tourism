7ac3046dd8eb3c37626a8b65e478ed5535518ede
akelios47 <47563558+akelios47@users.noreply.github.com>
2021-06-25 00:33:11 +0300

    Index aws fix attempt 2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
 // Index Page
-router.get('/', forwardAuthenticated, (req, res) => res.render('./routes/index.js'));
+router.get('/', forwardAuthenticated, (req, res) => res.render('./views/index.ejs'));
 
 // Dashboard
 router.get('/dashboard', ensureAuthenticated, (req, res) =>

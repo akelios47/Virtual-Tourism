5151e2b6bc25d570e8e72bfaee91aebe346576a6
akelios47 <47563558+akelios47@users.noreply.github.com>
2021-08-25 15:42:03 +0300

    Minor refinments and btns
    - Added buttons in tyre

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-dbPassword = 'mongodb+srv://lebanese_dev:'+ encodeURIComponent('0NvT8n3YEIGw9UwT') + '@cluster0.7efeq.mongodb.net/test?retryWrites=true';
+//dbPassword = 'mongodb+srv://lebanese_dev:'+ encodeURIComponent('0NvT8n3YEIGw9UwT') + '@cluster0.7efeq.mongodb.net/test?retryWrites=true';
+dbPassword = 'mongodb://localhost:27017/VTDB';
 
 module.exports = {
     mongoURI: dbPassword
